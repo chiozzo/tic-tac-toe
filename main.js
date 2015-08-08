235ce238a0dd2ee873c483943e18a91a57caffa0
Allen Phelps <allenatmc@gmail.com>
2015-08-08 10:04:45 -0500

    added event listeners, sadly deleted jQuery : (

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-$('#board').css('color', 'red');
+/*
+Variables for DOM elements
+*/
+
+var board = document.getElementById("board");
+var squares = board.getElementsByTagName('div');
+
+
+
+
+
+/*
+
+Add Event Handler for Clicking on Divs
+
+*/
+
+function clicked(e){
+console.log("You clicked " + this);
+};
+
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener('click', clicked);
+};
