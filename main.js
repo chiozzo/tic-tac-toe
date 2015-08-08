2dd1eaf832b884426040f3fb42a3ec25cb0a43c5
Martin Cross <mncross@gmail.com>
2015-08-08 10:27:50 -0500

    added reset button

--- main.js
+++ main.js
@@ -8,7 +8,25 @@ var squares = board.getElementsByTagName('div');
 var player1 = true;
 var player2 = false;
 
+var reset = document.getElementById("reset");
 
+
+/*
+Clicked event handler add in X & O and switch player
+*/
+
+function clicked(e){
+  console.log("You clicked " + this);
+  if (player1) {
+    this.innerText = "O";
+    player1 = false;
+    player2 = true
+  } else {
+    this.innerText = "X";
+    player2 = false;
+    player1 = true;
+  }
+};
 
 /*
 
@@ -16,19 +34,30 @@ Add Event Handler for Clicking on Divs
 
 */
 
-function clicked(e){
-console.log("You clicked " + this);
-if (player1) {
-  this.innerText = "O";
-  player1 = false;
-  player2 = true
-} else {
-  this.innerText = "X";
-  player2 = false;
-  player1 = true;
-}
+for (var i = 0; i < squares.length; i++) {
+  squares[i].addEventListener('click', clicked);
 };
 
-for (var i = 0; i < squares.length; i++) {
-	squares[i].addEventListener('click', clicked);
+/*
+Add event handler for button to reset board
+*/
+
+function reset(e) {
+  console.log("reset called");
+  for (var i = 0; i < squares.length; i++) {
+  squares[i].innerText = " ";
 };
+
+}
+
+reset.addEventListener('click', reset);
+
+
+
+
+
+
+
+
+
+
