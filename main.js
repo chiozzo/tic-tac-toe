49403640d2deccddaa32c88cf0e38084f97faaf3
Martin Cross <mncross@gmail.com>
2015-08-08 10:16:38 -0500

    added player variables

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ Variables for DOM elements
 var board = document.getElementById("board");
 var squares = board.getElementsByTagName('div');
 
-
+var player1 = true;
+var player2 = false;
 
 
 
@@ -17,6 +18,15 @@ Add Event Handler for Clicking on Divs
 
 function clicked(e){
 console.log("You clicked " + this);
+if (player1) {
+  this.innerText = "O";
+  player1 = false;
+  player2 = true
+} else {
+  this.innerText = "X";
+  player2 = false;
+  player1 = true;
+}
 };
 
 for (var i = 0; i < squares.length; i++) {
